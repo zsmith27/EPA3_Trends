4e7f68883102f1123ba781deb16c548923b6113f
Zachary Smith <zsmith@icprb.int>
2017-07-10 08:21:13 -0400

    Originally, the Water Quality Portal (WQP) data was subset to only include stations that were identified as long-term stations using the Legacy STORET data. However, long-term data, which was not reported in Legacy STORET, exists in the WQP data set. The script was updated to simply join the individual WQP data sets and export them as a csv.

--- WQP/wqp_station_subset.R
+++ WQP/wqp_station_subset.R
@@ -1,66 +1,55 @@
 #==============================================================================
 #==============================================================================
 # Author: Zachary M. Smith
-# Created: 1/10/17
-# Updated: 1/26/17
+# Created: 01/10/17
+# Updated: 07/10/17
 # Maintained: Zachary M. Smith
 # Purpose: The script was written to subset the WQP data to only include
 #          data from 1965 to present and long-term monitoring stations 
-#          identified in the Legacy STORET.
-# Output: Creates individual ".txt" files for each EPA Region 3 State.
+#          identified in the Legacy STORET. The script has since been updated
+#          to simply join the seperate WQP data.
+# Output: Creates csv file of all of the WQP data.
 #==============================================================================
 #==============================================================================
-# Load stations and parameters specified by Buchanan.
-setwd("//Pike/data/Projects/EPA3Trends/Data/Data_Jan2017/Legacy/Claire_Specifications")
-#stations.df <- read.table("LegacyStationsAll_050316Claire.txt", header = TRUE)
-stations.df <- read.csv("legacy_nwis_stations_1_26_2017.csv")
-#==============================================================================
+# Load dplyr for data manipulation.
+library(dplyr)
+#------------------------------------------------------------------------------
 # Set working directory.
-setwd("//Pike/data/Projects/EPA3Trends/Data/WQP/Subset Raw Data")
-#==============================================================================
+main.dir <- "//Pike/data/Projects/EPA3Trends/Data/WQP/Subset Raw Data"
+#------------------------------------------------------------------------------
+# Specify columns to be read as class "Character".
+char.cols <- "DetectionQuantitationLimitMeasure.MeasureValue"
 # Import the data for each state. 
 # The data was previously subset to only include potential params of interest.
-dc.df <- data.table::fread("sub_wqp_dc.csv", stringsAsFactors = FALSE)
-de.df <- data.table::fread("sub_wqp_de.csv", stringsAsFactors = FALSE)
-md.df <- data.table::fread("sub_wqp_md.csv", stringsAsFactors = FALSE)
-pa.df <- data.table::fread("sub_wqp_pa.csv", stringsAsFactors = FALSE)
-va.df <- data.table::fread("sub_wqp_va.csv", stringsAsFactors = FALSE)
-wv.df <- data.table::fread("sub_wqp_wv.csv", stringsAsFactors = FALSE)
-#==============================================================================
-# This function subsets the WQP data to only include data collected after 1965
-# and stations selected in the Legacy STORET data.
-keep_stations <- function(org.df, sta.df){
-  org.df$ActivityStartDate <- as.Date(org.df$ActivityStartDate)
-  #test <- org.df[org.df$ActivityStartDate >= "1965-01-01", ]
-  final.df <- org.df[grepl(paste(sta.df$Station, collapse = "|"),
-                         org.df$MonitoringLocationIdentifier), ]
-  return(final.df)
-}
-#==============================================================================
-# Apply the "keep_stations" function.
-dc <- keep_stations(dc.df, stations.df)
-de <- keep_stations(de.df, stations.df)
-md <- keep_stations(md.df, stations.df)
-pa <- keep_stations(pa.df, stations.df)
-va <- keep_stations(va.df, stations.df)
-wv <- keep_stations(wv.df, stations.df)
-#==============================================================================
+dc <- data.table::fread(file.path(main.dir, "sub_wqp_dc.csv"),
+                        colClasses = list(character = char.cols))
+de <- data.table::fread(file.path(main.dir, "sub_wqp_de.csv"),
+                        colClasses = list(character = char.cols))
+md <- data.table::fread(file.path(main.dir, "sub_wqp_md.csv"),
+                        colClasses = list(character = char.cols))
+pa <- data.table::fread(file.path(main.dir, "sub_wqp_pa.csv"),
+                        colClasses = list(character = char.cols))
+va <- data.table::fread(file.path(main.dir, "sub_wqp_va.csv"),
+                        colClasses = list(character = char.cols))
+wv <- data.table::fread(file.path(main.dir, "sub_wqp_wv.csv"),
+                        colClasses = list(character = char.cols))
+#------------------------------------------------------------------------------
 # Join all of the station information into a single data frame.
-bound <- rbind(dc, de, md, pa, va, wv)
-#==============================================================================
+bound <- bind_rows(dc, de, md, pa, va, wv)
+#------------------------------------------------------------------------------
 # Load the WQP station information
-setwd("//Pike/data/Projects/EPA3Trends/Data/WQP/Subset Raw Data/Site_Data")
-wqp.station <- read.csv("sub_site_all.csv")
-#==============================================================================
+station.dir <- "//Pike/data/Projects/EPA3Trends/Data/WQP/Subset Raw Data/Site_Data"
+wqp.station <- data.table::fread(file.path(station.dir, "sub_site_all.csv"))
+#------------------------------------------------------------------------------
 # Join the site info with the param data.
 by.names <- c("OrganizationIdentifier", "OrganizationFormalName",
               "MonitoringLocationIdentifier") #, "MonitoringLocationName")
 final.df <- dplyr::left_join(bound, wqp.station, by = by.names )
-#==============================================================================
+#------------------------------------------------------------------------------
 # Export all data as a single table.
-setwd("//Pike/data/Projects/EPA3Trends/Data/WQP/Subset Raw Data")
-write.csv(final.df, "wqp_keep_stations.csv", row.names = FALSE)
-
+export.dir <- "//Pike/data/Projects/EPA3Trends/Data/WQP/Subset Raw Data"
+data.table::fwrite(final.df, file.path(export.dir, "wqp_keep_stations.csv"))
+#------------------------------------------------------------------------------
 
 
 
